86887bf2f64beed6016f3fe2727505d67dc1e966
Jesús Camargo <acjesus@uninorte.edu.co>
2021-06-30 01:15:49 -0500

    Feat(Roulette): Add controller

---
+++ src/roulette/roulette.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('roulette')
+export class RouletteController {}
--- src/roulette/roulette.module.ts
+++ src/roulette/roulette.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RouletteController } from './roulette.controller';
 import { RouletteService } from './roulette.service';
 
 @Module({
+  controllers: [RouletteController],
   providers: [RouletteService],
 })
 export class RouletteModule {}
