ff8adfcb846c56ed1aee384d320f55ec9c97ef8e
Jesús Camargo <acjesus@uninorte.edu.co>
2021-06-30 13:22:44 -0500

    Feat(Roulette/Service): Add getter for all roulettes

--- src/roulette/roulette.service.ts
+++ src/roulette/roulette.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from 'nestjs-redis';
+import { Roulette } from './interfaces/roulette.interface';
 
 @Injectable()
-export class RouletteService {}
+export class RouletteService {
+  constructor(private readonly redisService: RedisService) {}
+
+  async findAll(): Promise<Roulette[]> {
+    const redisClient = this.redisService.getClient();
+    const roulettesString = await redisClient.hgetall('roulettes');
+    const roulettes: Roulette[] = Object.keys(roulettesString).map(rouletteId =>
+      JSON.parse(roulettesString[rouletteId]),
+    );
+
+    return roulettes;
+  }
+}
